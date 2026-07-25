d798e90ed2846e65333990a6480412d22b397e74
tunhienhieuchuyen <tunhienhieuchuyen@gmail.com>
2026-07-25 09:06:44 +0700

    Add files via upload

---
+++ facebook_danhsach.js
@@ -0,0 +1,32 @@
+var fbStaticPlaylist = [
+    {
+        "type": "facebook_reel",
+        "id": "2082570962469390",
+        "title": "Facebook Reel 01",
+        "url": "https://www.facebook.com/reel/2082570962469390"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1721240362452422",
+        "title": "Facebook Reel 02",
+        "url": "https://www.facebook.com/reel/1721240362452422"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1574301110709399",
+        "title": "Facebook Reel 03",
+        "url": "https://www.facebook.com/reel/1574301110709399"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "3271433006374486",
+        "title": "Facebook Reel 04",
+        "url": "https://www.facebook.com/reel/3271433006374486"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "2146022342610125",
+        "title": "Facebook Reel 05",
+        "url": "https://www.facebook.com/reel/2146022342610125"
+    }
+];
