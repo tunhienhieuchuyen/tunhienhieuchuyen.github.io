5302c3522832951717e85031aaaeb2d5a61f90e0
tunhienhieuchuyen <tunhienhieuchuyen@gmail.com>
2026-07-26 13:55:25 +0700

    Add files via upload

--- facebook_danhsach.js
+++ facebook_danhsach.js
@@ -1,32 +1,134 @@
 var fbStaticPlaylist = [
     {
         "type": "facebook_reel",
+        "id": "1564943745288882",
+        "title": "Bỏ ngay thói quen này nếu không muốn mãi là \"nấm lùn\" 🚫🥤",
+        "url": "https://www.facebook.com/reel/1564943745288882"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "2162573857651066",
+        "title": "Có thứ gì đang xuyên qua cơ thể bạn NGAY LÚC NÀY?! 🤯",
+        "url": "https://www.facebook.com/reel/2162573857651066"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "4377708115850582",
+        "title": "Dây điện cao thế nguy hiểm như vậy, tại sao những chú chim nhỏ bé vẫn có thể bình thản đứng trên đó?",
+        "url": "https://www.facebook.com/reel/4377708115850582"
+    },
+    {
+        "type": "facebook_reel",
         "id": "2082570962469390",
-        "title": "Facebook Reel 01",
+        "title": "Chỉ muỗi cái mới hút máu, còn muỗi đực chỉ đi hút mật hoa! Bạn có tin không? 🦟🌸",
         "url": "https://www.facebook.com/reel/2082570962469390"
     },
     {
         "type": "facebook_reel",
         "id": "1721240362452422",
-        "title": "Facebook Reel 02",
+        "title": "\"Cái này ăn được không?\" Sốc với loạt món ăn dị hợm để hàng chục năm không hỏng! 😱❌",
         "url": "https://www.facebook.com/reel/1721240362452422"
     },
     {
         "type": "facebook_reel",
         "id": "1574301110709399",
-        "title": "Facebook Reel 03",
+        "title": "🎈 BOM BÓNG BAY + 🩸 MÁU GIẢ = 😱 PHA \"HẤP HỐI\" ĐỂ ĐỜI!",
         "url": "https://www.facebook.com/reel/1574301110709399"
     },
     {
         "type": "facebook_reel",
         "id": "3271433006374486",
-        "title": "Facebook Reel 04",
+        "title": "Bí mật về 20.000 lần chớp mắt mỗi ngày: Bộ não đang tự động \"lừa\" thị giác của bạn! 👁️⚡",
         "url": "https://www.facebook.com/reel/3271433006374486"
     },
     {
         "type": "facebook_reel",
         "id": "2146022342610125",
-        "title": "Facebook Reel 05",
+        "title": "Đang đạp xe một mình giữa con đường tối, ngẩng đầu lên thấy mấy “bóng trắng” đứng im hai bên đường… 😨",
         "url": "https://www.facebook.com/reel/2146022342610125"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1798364531332504",
+        "title": "Mặt Trời bốc cháy suốt 4,6 tỷ năm dù Vũ trụ... không có một giọt oxy nào! Rốt cuộc chuyện gì đang xảy ra? ☀️🤯",
+        "url": "https://www.facebook.com/reel/1798364531332504"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "2519681408483088",
+        "title": "Bị ong đốt sưng tấy? Tuyệt đối CẤM làm hành động này kẻo nọc độc thấm sâu hơn! 🐝❌",
+        "url": "https://www.facebook.com/reel/2519681408483088"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1741858730168508",
+        "title": "30 phút đi bộ thong thả lại gọt mỡ \"đỉnh\" hơn 30 phút chạy thục mạng! Bạn có tin không? 🏃‍♂️❌🚶‍♂️✅",
+        "url": "https://www.facebook.com/reel/1741858730168508"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1422362039728997",
+        "title": "Nhặt vỏ ốc lên áp vào tai và nghe thấy tiếng \"sóng biển\" rì rào? Sự thật sẽ khiến bạn ngã ngửa vì vỏ ốc không hề biết phát ra âm thanh đâu! Cùng xem m",
+        "url": "https://www.facebook.com/reel/1422362039728997"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1402899371651747",
+        "title": "Bạn nghĩ tia sét cứ thế đánh thẳng từ trên trời xuống đất? Sai lầm rồi! 🌩️ Bầu trời thực chất là một \"mê cung\" khổng lồ, và tia sét phải liên tục dò đ",
+        "url": "https://www.facebook.com/reel/1402899371651747"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1824082001897146",
+        "title": "Cú \"chôm bô\" đi vào lịch sử và phát minh phân bón NPK của Anh Hiểu Chuyện! 🤣💥",
+        "url": "https://www.facebook.com/reel/1824082001897146"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "2205471710237326",
+        "title": "Ngồi ăn Tất niên mà làm mấy hành động này thì xác định... dọn sẵn roi mây \"tới công chuyện\" với phụ huynh! 🥢❌",
+        "url": "https://www.facebook.com/reel/2205471710237326"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1284932693717199",
+        "title": "Nguyên nhân thật sự của động đất - Có thể bạn chưa biết! 🛑",
+        "url": "https://www.facebook.com/reel/1284932693717199"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "2224506745033964",
+        "title": "🚨 Bữa ăn khuya đang \"đánh cắp\" sự sống của bạn như thế nào? 😱",
+        "url": "https://www.facebook.com/reel/2224506745033964"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1375207368083396",
+        "title": "Vũ trụ kỳ diệu hơn những gì chúng ta tưởng tượng rất nhiều... 🌌✨",
+        "url": "https://www.facebook.com/reel/1375207368083396"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "3634185890086804",
+        "title": "Đã bao giờ bạn khóc xong rồi... thấy mọi chuyện vẫn y nguyên chưa? 🥲",
+        "url": "https://www.facebook.com/reel/3634185890086804"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "839482769100728",
+        "title": "Vì sao ong thấy hoa cải hơi đắng?",
+        "url": "https://www.facebook.com/reel/839482769100728"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1355680425944294",
+        "title": "Vì sao sau cơn mưa lại có mùi đất rất dễ chịu?",
+        "url": "https://www.facebook.com/reel/1355680425944294"
+    },
+    {
+        "type": "facebook_reel",
+        "id": "1044413581413301",
+        "title": "Tại sao chúng mình cần đánh răng trước khi đi ngủ nhỉ?",
+        "url": "https://www.facebook.com/reel/1044413581413301"
     }
 ];
