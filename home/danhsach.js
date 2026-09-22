ccb8bd441a9f08a2cf12c7aea89a4c55a6236783
tunhienhieuchuyen <tunhienhieuchuyen@gmail.com>
2026-09-22 14:22:56 +0700

    Add files via upload

---
+++ home/danhsach.js
@@ -0,0 +1,172 @@
+var ytStaticPlaylist = [
+    {
+        "type": "video",
+        "id": "p7HschsPpN4",
+        "title": "🧠 Giấc Ngủ: \"Nhà Máy Chữa Lành\" Miễn Phí Của Cơ Thể! #Shorts"
+    },
+    {
+        "type": "video",
+        "id": "NRb0Xk2pUrE",
+        "title": "🚨 Sai Lầm Chết Người Khi Sơ Cứu Điện Giật! Xem Ngay #Shorts"
+    },
+    {
+        "type": "video",
+        "id": "Z6SGrD2c6vI",
+        "title": "Uống 1 cốc nước ấm mỗi sáng, cơ thể thay đổi ngỡ ngàng! ☕️💧 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "qF4xt0u_xyE",
+        "title": "99% Mọi Người Đều Bỏ Qua những Thói Quen Nhỏ Này Khi Ăn trong bữa ăn hằng ngày! 🍽️ #shorts"
+    },
+    {
+        "type": "video",
+        "id": "YExX4ntPojQ",
+        "title": "Đừng Thức Khuya Nữa! Cơ Thể Bạn Đang \"Lão Hóa\" Nhanh Chóng Như Thế Nào? #shorts"
+    },
+    {
+        "type": "video",
+        "id": "amPVT8zJqQA",
+        "title": "Top \"thần dược\" tự nhiên giúp da trắng mịn, chống lão hóa cực đỉnh! ✨ #shorts"
+    },
+    {
+        "type": "video",
+        "id": "PZpqcWR7NG8",
+        "title": "Thói Quen Uống Nước Đang Âm Thầm Phá Hỏng Dạ Dày Của Bạn! #shorts"
+    },
+    {
+        "type": "video",
+        "id": "YVjOcjIgf9k",
+        "title": "Sự thật rợn tóc gáy về những \"vết bẩn\" trên mũi mà ít ai biết! 🔍 😱 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "ANVeSp0701k",
+        "title": "Bấm báo thức lần 2 độc hại cỡ nào? Sự thật khoa học về giấc ngủ! ⏰💤 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "i3ZJcxMwOBk",
+        "title": "Đặt bàn chải ở chỗ này là nuôi cả ổ vi khuẩn cực khủng! #shorts"
+    },
+    {
+        "type": "video",
+        "id": "T8EIPJthoVI",
+        "title": "90% người không biết quy tắc truyền máu này! Bạn có thuộc số đó? 🤔 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "4a4pjkhScz8",
+        "title": "Hiểu Chuyện đã cho bạn thấy kết cấu thật sự của một cái giếng! 🕳️💧 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "4oZ4gHynHIU",
+        "title": "Tại sao chúng ta lại đổ mồ hôi khi đá bóng dưới trời nắng? 🥵 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "BGHzMTIRuXg",
+        "title": "Tại sao nước biển lại mặn? Sự thật khổng lồ không ai ngờ tới! 🌊#shorts"
+    },
+    {
+        "type": "video",
+        "id": "HC5h46tO52w",
+        "title": "Bỏ ngay thói quen này nếu không muốn mãi là \"nấm lùn\" 🚫🥤 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "1sRMHohYWWw",
+        "title": "Có thứ gì đang xuyên qua cơ thể bạn NGAY LÚC NÀY?! 🤯 #shorts"
+    },
+    {
+        "type": "video",
+        "id": "nall6__hoFc",
+        "title": "Vì sao chim đậu trên dây điện cao thế mà không bị giật? #shorts"
+    },
+    {
+        "type": "video",
+        "id": "iDcYQXmBgAk",
+        "title": "Sự Thật Bất Ngờ: Tại Sao Chỉ Có Muỗi Cái Mới Hút Máu? 🦟🩸#shorts"
+    },
+    {
+        "type": "video",
+        "id": "S6lmkU3GGqg",
+        "title": "Món Ăn \"Dị\" Nhất Khiến Bạn Nổi Da Gà! #shorts"
+    },
+    {
+        "type": "video",
+        "id": "U1PV96HOSP0",
+        "title": "BỊ ĐÁNH THỨC BỞI BOM BÓNG BAY VÀ CÁI KẾT BẤT NGỜ #shorts"
+    },
+    {
+        "type": "video",
+        "id": "ySGNsGtGwrg",
+        "title": "Chớp mắt Bộ Não Đang \"Lừa\" Bạn 20.000 Lần Mỗi Ngày! 👁️🧠"
+    },
+    {
+        "type": "video",
+        "id": "zi9U5YAPEvY",
+        "title": "Mặt Trời Không Dùng Oxy, Vậy Nó \"Bốc Cháy\" Bằng Cách Nào? ☀️⚛️#shorts"
+    },
+    {
+        "type": "video",
+        "id": "g9R4ykD2MOA",
+        "title": "Mẹo Xử Lý Vết Ong Đốt Sưng Tấy Cực Nhanh! 🐝🩹#shorts"
+    },
+    {
+        "type": "video",
+        "id": "riOY995dlyc",
+        "title": "Đi Bộ Hay Chạy Bộ Giảm Mỡ Nhanh Hơn? Sự Thật Bất Ngờ! 🏃‍♂️🔥#shorts"
+    },
+    {
+        "type": "video",
+        "id": "uSngpnGNoPc",
+        "title": "Bí Ẩn Tiếng \"Sóng Biển\" Trong Vỏ Ốc - Tuổi Thơ Bị Lừa Rồi! 🐚🌊#shorts"
+    },
+    {
+        "type": "video",
+        "id": "JzzWuy37rfY",
+        "title": "Tại Sao Tia Sét Lại Ngoằn Ngoèo Chứ Không Thẳng Tắp? ⚡🌩️#shorts"
+    },
+    {
+        "type": "video",
+        "id": "iW3r7FIvaek",
+        "title": "Cú \"chôm bô\" đi vào lịch sử và phát minh phân bón NPK của Anh Hiểu Chuyện!"
+    },
+    {
+        "type": "video",
+        "id": "O4AaHSWO2s0",
+        "title": "Đại Kỵ Dùng Đũa Trên Bàn Tiệc - Cẩn Thận Kẻo \"Ăn Đòn\"! hiểu chuyện #shorts"
+    },
+    {
+        "type": "video",
+        "id": "rE6F09pCFQY",
+        "title": "Động Đất Xảy Ra Như Thế Nào? Bí Mật Dưới Lòng Đất! 🌍💥#shorts"
+    },
+    {
+        "type": "video",
+        "id": "CS_7byroeCA",
+        "title": "Sự Thật Đáng Sợ Về Thói Quen Ăn Đêm 😱 tunhienhieuchuyen #shorts"
+    },
+    {
+        "type": "video",
+        "id": "SvYIQufvCqQ",
+        "title": "Vì sao khóc không phải là cách để giải quyết vấn đề #shorts"
+    },
+    {
+        "type": "video",
+        "id": "uXFnERktJek",
+        "title": "Bạn đã từng thấy mặt trời kỳ lạ này chưa? Vì sao lại thế #shorts"
+    },
+    {
+        "type": "video",
+        "id": "SmgjfmbplSM",
+        "title": "Có thể bạn nhìn thấy nó mỗi ngày nhưng chưa từng hiểu đúng #shorts"
+    },
+    {
+        "type": "video",
+        "id": "0JLxdN-oP3E",
+        "title": "Vì sao chúng ta lại xì hơi? Sự thật ai cũng từng gặp #shorts"
+    }
+];
